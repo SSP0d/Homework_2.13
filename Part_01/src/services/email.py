b62cf49db5a19cd8730578196eff0496e05c905e
Serhii Pidkopai <spodkopay@gmail.com>
2023-03-03 00:40:35 +0200

    Update email.py

--- Part_01/src/services/email.py
+++ Part_01/src/services/email.py
@@ -1,4 +1,16 @@
+from pathlib import Path
+
+import uvicorn
+from fastapi import FastAPI, BackgroundTasks
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
+from pydantic import EmailStr, BaseModel
+
 from src.config import settings
+
+
+class EmailSchema(BaseModel):
+    email: EmailStr
+
 
 conf = ConnectionConfig(
     MAIL_USERNAME=settings.mail_username,
@@ -13,3 +25,26 @@ conf = ConnectionConfig(
     VALIDATE_CERTS=True,
     TEMPLATE_FOLDER=Path(__file__).parent / 'templates',
 )
+
+app = FastAPI()
+
+
+@app.post("/send-email")
+async def send_in_background(background_tasks: BackgroundTasks, body: EmailSchema):
+    message = MessageSchema(
+        subject="Fastapi mail module",
+        recipients=[body.email],
+        template_body={"fullname": "Billy Jones"},
+        subtype=MessageType.html
+    )
+
+    fm = FastMail(conf)
+
+    background_tasks.add_task(fm.send_message, message, template_name="example_email.html")
+
+    return {"message": "email has been sent"}
+
+
+if __name__ == '__main__':
+    uvicorn.run('email:app', port=8000, reload=True)
+    
