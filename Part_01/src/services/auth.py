439ab2adb89f1199695e592e97c07757b3006b7d
Serhii Pidkopai <spodkopay@gmail.com>
2023-03-14 10:20:08 +0200

    service auth ref

--- Part_01/src/services/auth.py
+++ Part_01/src/services/auth.py
@@ -18,7 +18,7 @@ class Auth:
     SECRET_KEY = settings.secret_key
     ALGORITHM = settings.algorithm
     oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/api/auth/login')
-    r = redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0)
+    redis_cache = redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0)
 
     def verify_password(self, plain_password, hashed_password):
         return self.pwd_context.verify(plain_password, hashed_password)
@@ -77,13 +77,13 @@ class Auth:
         except JWTError as e:
             raise credentials_exception
 
-        user = self.r.get(f"user:{email}")
+        user = self.redis_cache.get(f"user:{email}")
         if user is None:
             user = await repository_users.get_user_by_email(email, db)
             if user is None:
                 raise credentials_exception
-            self.r.set(f"user:{email}", pickle.dumps(user))
-            self.r.expire(f"user:{email}", 900)
+            self.redis_cache.set(f"user:{email}", pickle.dumps(user))
+            self.redis_cache.expire(f"user:{email}", 900)
         else:
             user = pickle.loads(user)
         return user
