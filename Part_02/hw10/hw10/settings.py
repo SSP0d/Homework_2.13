e4d0890ae3d89d5ea5ad0bdc3ea652001ac7fb7e
Serhii Pidkopai <spodkopay@gmail.com>
2023-03-14 12:34:12 +0200

    Update settings.py

--- Part_02/hw10/hw10/settings.py
+++ Part_02/hw10/hw10/settings.py
@@ -12,7 +12,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-import env
+import environ
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
